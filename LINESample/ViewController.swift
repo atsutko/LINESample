74dfacbf3f9c6824a871ecaf493dba7432814386
atsutko <atsushi.takao.66@gmail.com>
2018-04-28 02:08:49 +0900

    Initial Commit

---
+++ LINESample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LINESample
+//
+//  Created by TakaoAtsushi on 2018/04/28.
+//  Copyright © 2018年 TakaoAtsushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
